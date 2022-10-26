4d39f931e36f369b8c5e9156d79d095f26abf53c
Ion-Alexandru Secara <ion.alexandru.secara@gmail.com>
2022-10-25 20:20:28 -0700

    Does this work

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
           <img src={bicepsImage} className="biceps-image reverted-biceps-image" />
         </div>
         {/* <video id="video" className="video-demo" autoplay></video> */}
-        <div dangerouslySetInnerHTML={{
+        {/* <div dangerouslySetInnerHTML={{
             __html: `
               <video
                 loop
@@ -38,16 +38,13 @@ class App extends Component {
                 autoplay
                 playsinline
               >
-                <source src={pizzaVideo} type="video/mp4"/>
+                <source src="./resources/pizza.mp4" type="video/mp4"/>
               </video
             `
           }}
-        />
-        <video src="https://app.kemtai.com/media/exercises/11526b5e-b661-41f7-a0ca-509ef80d3f7f/480p.webm#t=0" class="playing-video" autoplay preload="auto" playsinline loop style="opacity: 1;"></video>
-        {/* <video autoPlay loop muted className="playing-video">
-          <source src={pizzaVideo} type="video/mp4"/>
-          Why isn't the video playing?
-        </video> */}
+        /> */}
+        {/* <video src="https://app.kemtai.com/media/exercises/11526b5e-b661-41f7-a0ca-509ef80d3f7f/480p.webm#t=0" class="playing-video" autoplay preload="auto" playsinline loop style="opacity: 1;"></video> */}
+        <video autoPlay loop muted className="playing-video" preload="auto" playsinline src="https://app.kemtai.com/media/exercises/11526b5e-b661-41f7-a0ca-509ef80d3f7f/480p.webm#t=0"/>
       </div>
     );
   }
