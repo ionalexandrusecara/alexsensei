8f820e6d5f0143dd442bd59101aea8354aef7db7
Ion-Alexandru Secara <ion.alexandru.secara@gmail.com>
2022-10-25 20:00:54 -0700

    Added check

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ class App extends Component {
           <img src={smileyImage} className="smiley-image" />
           <img src={bicepsImage} className="biceps-image reverted-biceps-image" />
         </div>
-        <video id="video" className="video-demo" autoplay></video>
-        <video autoPlay className="playing-video">
+        {/* <video id="video" className="video-demo" autoplay></video> */}
+        <video controls autoPlay className="playing-video">
           <source src={pizzaVideo} type="video/mp4"/>
           Why isn't the video playing?
         </video>
