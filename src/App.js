5353a1c9130d4cef7099cfff9b3f9de202b9bc51
Ion-Alexandru Secara <ion.alexandru.secara@gmail.com>
2022-10-25 20:32:28 -0700

    check 7

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
           <img src={bicepsImage} className="biceps-image reverted-biceps-image" />
         </div>
         {/* <video id="video" className="video-demo" autoplay></video> */}
-        <div dangerouslySetInnerHTML={{
+        {/* <div dangerouslySetInnerHTML={{
             __html: `
               <video
                 autoPlay
@@ -43,9 +43,12 @@ class App extends Component {
               />
             `
           }}
-        />
+        /> */}
         {/* <video src="https://app.kemtai.com/media/exercises/11526b5e-b661-41f7-a0ca-509ef80d3f7f/480p.webm#t=0" class="playing-video" autoplay preload="auto" playsinline loop style="opacity: 1;"></video> */}
         <video autoPlay loop muted className="playing-video" preload="auto" playsinline src={pizzaVideo}/>
+        <p className="hello-text">
+          Really?
+        </p>
       </div>
     );
   }
