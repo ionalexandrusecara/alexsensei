4f2701d67280073baa543cc8399176bad29dcab1
Ion-Alexandru Secara <ion.alexandru.secara@gmail.com>
2022-10-26 19:29:52 -0700

    Latest, so I don't forget

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import bicepsImage from './resources/biceps.png';
 import smileyImage from './resources/smiley.png';
 import pizzaVideo from './resources/pizza.webm';
+import pizzaVideo2 from './resources/pizza3.mp4';
+import Webcam from "react-webcam";
 
 import './App.scss';
 import { loadWebcamVideo } from './services/cameraService';
@@ -29,8 +31,9 @@ class App extends Component {
           <img src={smileyImage} className="smiley-image" />
           <img src={bicepsImage} className="biceps-image reverted-biceps-image" />
         </div>
+        <Webcam width={300} height={300}/>
         {/* <video id="video" className="video-demo" autoplay></video> */}
-        {/* <div dangerouslySetInnerHTML={{
+        <div dangerouslySetInnerHTML={{
             __html: `
               <video
                 autoPlay
@@ -39,16 +42,19 @@ class App extends Component {
                 className="playing-video"
                 preload="auto"
                 playsinline
-                src=${pizzaVideo}
+                width="300"
+                height="300"
+                src=${pizzaVideo2}
               />
             `
           }}
-        /> */}
+        />
         {/* <video src="https://app.kemtai.com/media/exercises/11526b5e-b661-41f7-a0ca-509ef80d3f7f/480p.webm#t=0" class="playing-video" autoplay preload="auto" playsinline loop style="opacity: 1;"></video> */}
-        <video autoPlay loop muted className="playing-video" playsinline src={pizzaVideo}/>
-        <p className="hello-text">
-          Really 2?
-        </p>
+        {/* <video controls autoPlay loop muted preload="auto" playsinline className="playing-video" src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"/> */}
+        {/* <video className="playing-video" src={pizzaVideo}/> */}
+        {/* <p className="hello-text">
+          Really 16?
+        </p> */}
       </div>
     );
   }
